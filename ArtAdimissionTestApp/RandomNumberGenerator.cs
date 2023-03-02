608f87b5cd31f2d93f38deac8d48fa4a4f48659d
Diego Corredor <diego.corredor.salinas@gmail.com>
2023-03-01 20:24:33 -0500

    - Se simplifica la clase GenerateRandomNumber - Después de hacer pruebas, se cambia el mecanismo para evitar InvalidKeyException (se estaba haciendo try cartchde OverFlowException pero no se levantaba la excepción, probocando un InvalidKeyException)

--- ArtAdimissionTestApp/RandomNumberGenerator.cs
+++ ArtAdimissionTestApp/RandomNumberGenerator.cs
@@ -12,9 +12,10 @@ namespace ArtAdimissionTestApp
     internal class RandomNumberGenerator
     {
         // miembros privados
-        private int[] randomNumber = { 1, 2, 3, 4, 5, 6, 7 }; // arreglo con las posibles opciones
         private Random numberGenerator = new Random(); // objeto random para la generacion de números aleatorios
         private int AcumulatedNumber { get; set; } = DateTime.Now.Second;
+        
+        // métodos públicos
         /// <summary>
         /// Método publico para la genreación de números aleatorios entre 1 y 7
         /// Parámetros de entrada: Sin parámetros
@@ -25,18 +26,12 @@ namespace ArtAdimissionTestApp
             int oneToFiveNumber = numberGenerator.Next(1, 6); // Se genera un numero aleatorio de 1 a 5
             
             // Se atiende la posible excepción de overflow de variable tipo int
-            try
-            {
-                AcumulatedNumber += oneToFiveNumber; //Se suma el valor generado entre 1 y 5  al numero acumulado
-            }
-            catch (OverflowException)
-            {
-                AcumulatedNumber = DateTime.Now.Second; // Se reinicia AcumulatedNumber con el numero de segundos de la hora actual
-
-            }
-
-            return randomNumber[AcumulatedNumber % 7]; // se obtiene un numero aleatorio entre 1 y 7
+            AcumulatedNumber += oneToFiveNumber; //Se suma el valor generado entre 1 y 5  al numero acumulado
+            if (AcumulatedNumber < 0)
+                AcumulatedNumber = DateTime.Now.Second;                       
+            return (AcumulatedNumber % 7) + 1; // se obtiene un numero aleatorio entre 1 y 7
         }
+
         /// <summary>
         /// Método para probar el generador de números aleatorios
         /// Se prueba el método "test" veces y entrega un sting con el reporte de la ocurrencia de cada número
